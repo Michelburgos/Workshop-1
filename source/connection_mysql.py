8a84048bff3cd6ee6f9b7526c0106cf04309913d
Michelburgos <143130589+Michelburgos@users.noreply.github.com>
2025-03-03 00:11:22 -0500

    EDA

--- source/connection_mysql.py
+++ source/connection_mysql.py
@@ -12,18 +12,15 @@ db_name = os.getenv('DB_NAME')
 db_port = os.getenv('DB_PORT')
 
 def get_sqlalchemy_engine():
-    """Crea y devuelve un engine de SQLAlchemy para conectar con MySQL"""
     try:
         engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
         
-        # Verificar conexión con un simple ping
         with engine.connect() as connection:
-            connection.execute(text("SELECT 1"))  # 🔹 Se usa text() para evitar el error
-
-        print("✅ Conexión a la base de datos exitosa.")
+            connection.execute(text("SELECT 1"))  
+        print("Conexión a la base de datos exitosa.")
         return engine
     
     except Exception as e:
-        print(f"❌ Error al conectar con la base de datos: {e}")
+        print(f"Error al conectar con la base de datos: {e}")
         return None
 
